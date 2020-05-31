1c95f800092ebd832500e648abec18e4fb12ec83
Amelie121212 <amel.1989ab@gmail.com>
2020-05-31 05:43:44 +0100

    2nd commit

---
+++ script.js
@@ -0,0 +1,23 @@
+$(".txt").on("keyup",function(e){
+    if(e.keyCode == 13 && $(".txt").val() != "")
+    {
+      var task = $("<div class='task'></div>").text($(".txt").val());
+      var del = $("<i class='fas fa-trash-alt'></i>").click(function(){
+        var p = $(this).parent();
+        p.fadeOut(function(){
+          p.remove();
+        });
+      });
+      var check = $("<i class='fas fa-check'></i>").click(function(){
+        var p = $(this).parent();
+        p.fadeOut(function(){
+          $(".comp").append(p);
+          p.fadeIn();
+        });
+        $(this).remove();
+      });
+      task.append(del,check);
+      $(".notcomp").append(task);
+      $(".txt").val("");
+    }
+});
