fc92ed782132154f681a1026467e0aa104de5496
Amelie121212 <amel.1989ab@gmail.com>
2020-06-01 09:50:54 +0100

    3rd commit

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-$(".txt").on("keyup",function(e){
-    if(e.keyCode == 13 && $(".txt").val() != "")
-    {
-      var task = $("<div class='task'></div>").text($(".txt").val());
-      var del = $("<i class='fas fa-trash-alt'></i>").click(function(){
-        var p = $(this).parent();
-        p.fadeOut(function(){
-          p.remove();
-        });
+$(".txt").on("keyup", function(e) {
+  if (e.keyCode == 13 && $(".txt").val() != "") {
+    $(".notcomp").css("display", "block");
+    var task = $("<div class='task'></div>").text($(".txt").val());
+    var del = $("<i class='fas fa-trash-alt'></i>").click( function() {
+      var p = $(this).parent();
+      p.fadeOut(function(){
+        p.remove();
       });
-      var check = $("<i class='fas fa-check'></i>").click(function(){
-        var p = $(this).parent();
-        p.fadeOut(function(){
-          $(".comp").append(p);
-          p.fadeIn();
-        });
-        $(this).remove();
+    });
+    var check = $("<i class='fas fa-check'></i>").click( function(){
+      var p = $(this).parent();
+      p.fadeOut( function() {
+        $(".comp").css("display", "block");
+        $(".comp").append(p);
+        p.fadeIn();
       });
-      task.append(del,check);
-      $(".notcomp").append(task);
-      $(".txt").val("");
-    }
+      $(this).remove();
+    });
+    task.append(del,check);
+    $(".notcomp").append(task);
+    $(".txt").val("");
+  }
 });
