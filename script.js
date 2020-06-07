68890c7c94991682f99ff7c6a191b333f184d25f
Amelie121212 <amel.1989ab@gmail.com>
2020-06-07 22:36:10 +0100

    Fix a bug

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 // declaire variable
-var notComp = JSON.parse(localStorage.getItem("notComp"));
-var compl = JSON.parse(localStorage.getItem("Comp"));
-var check = $("<i class='fas fa-check'></i>");
-var del = $("<i class='fas fa-trash-alt'></i>");
-var task = $("<div class='task'></div>");
 
+// create task
 $( document ).ready(function() {
   // Time and Date
   setInterval(() => {
@@ -14,7 +10,7 @@ $( document ).ready(function() {
     }
     var dt = new Date();
     var time = addZero(dt.getHours()) + " : " + addZero(dt.getMinutes());
-    var date = addZero(dt.getDay()) + " - " + addZero(dt.getMonth()) + " - " +dt.getFullYear();
+    var date = addZero(dt.getDate()) + " - " + addZero(dt.getMonth()) + " - " +dt.getFullYear();
     $("#time").html(time);
     $("#date").html(date);
   }, 1000);
@@ -25,6 +21,50 @@ $( document ).ready(function() {
   $("#pause").click(() => {
     $('#song').get(0).pause();
   });
+  var notComp = JSON.parse(localStorage.getItem("notComp"));
+  var compl = JSON.parse(localStorage.getItem("Comp"));
+  $(".txt").on("keyup", function(e) {
+    if (e.keyCode == 13 && $(".txt").val() != "") {
+      var task = $("<div class='task'></div>");
+      var check = $("<i class='fas fa-check'></i>");
+      var del = $("<i class='fas fa-trash-alt'></i>");
+      task.text($(".txt").val());    
+      task.append(del,check);
+      $(".notcomp").append(task);
+      $(".txt").val("");
+      // notComp.push({
+      //   title: $(".txt").val()
+      // });
+      // console.log("push", $(".txt").val());
+      // localStorage.setItem("notComp", JSON.stringify(notComp));
+    }
+    // Manage tasks - check -
+    check.click( function(){
+      var t = $(this).parent();
+      t.fadeOut( function() {
+        $(".comp").append(t);
+        t.fadeIn();
+        console.log(t,"to .comp");
+      });
+      $(this).remove();
+      // compl.push({
+      //   title: t[0].innerText
+      // });
+      // console.log("push", t[0].innerText);
+      // localStorage.setItem("Comp", JSON.stringify(compl));
+    });
+    // Manage tasks - delete -
+    del.click( function() {
+      var t = $(this).parent();
+      t.fadeOut(function(){
+        t.remove();
+        console.log(t,"removed");
+      });
+    });
+  });
+});
+
+  
   // Load storage
   // $.each(notComp, function(i, item) {
   //   let t= $("<div class='task'></div>").text(item.title);
@@ -37,42 +77,6 @@ $( document ).ready(function() {
   //   t.append($("<i class='fas fa-trash-alt'></i>"));
   //   $(".comp").append(t);
   // });
-  // Manage tasks - check -
-  check.click( function(){
-    var t = $(this).parent();
-    t.fadeOut( function() {
-      $(".comp").append(t);
-      t.fadeIn();
-      console.log(t,"to .comp");
-    });
-    $(this).remove();
-    // compl.push({
-    //   title: t[0].innerText
-    // });
-    // console.log("push", t[0].innerText);
-    // localStorage.setItem("Comp", JSON.stringify(compl));
-    
-  });
-  // Manage tasks - delete -
-  del.click( function() {
-    var t = $(this).parent();
-    t.fadeOut(function(){
-      t.remove();
-      console.log(t,"removed");
-    });
-  });
-  // create task
-  $(".txt").on("keyup", function(e) {
-    if (e.keyCode == 13 && $(".txt").val() != "") {
-      task.text($(".txt").val());    
-      task.append(del,check);
-      $(".notcomp").append(task);
-      $(".txt").val("");
-      // notComp.push({
-      //   title: $(".txt").val()
-      // });
-      // console.log("push", $(".txt").val());
-      // localStorage.setItem("notComp", JSON.stringify(notComp));
-    }
-  });
-});
+  
+  
+
